dc260d7d89ce5195095217a0e95066c6d6d92480
Stasiuk <stasiukyurys@gmail.com>
2020-11-10 14:35:07 +0300

    Начальная структура сайта. Подключение стиля bootstrap. Созданы страницы html. Главное меню.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -13,6 +13,31 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/help')
+def help_user():
+    return render_template('help.html')
+
+
+@app.route('/abk')
+def abk():
+    return render_template('abk.html')
+
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+
+@app.route('/production')
+def production():
+    return render_template('production.html')
+
+
+@app.route('/pit')
+def pit():
+    return render_template('pit.html')
 
 
 @app.route('/user/<string:name>/<int:id>')
